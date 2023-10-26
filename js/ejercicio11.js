eeaa17c56c28b18b3ce8dbc51a0718627bd1f99e
Natalia Lopez <natigab.24@gmail.com>
2023-10-26 15:24:26 -0600

    ejercicio 11 completo

---
+++ js/ejercicio11.js
@@ -0,0 +1,21 @@
+const numero1 = parseInt(prompt("Ingrese un numero"));
+
+if (numero1 % 2 == 0 || numero1 % 3 == 0 || numero1 % 5 == 0 || numero1 % 7 == 0) {
+  
+  if (numero1 % 2 == 0) {
+    document.write("<p>El numero " + numero1 + " es divisible por 2 </p></br>");
+  }
+  if (numero1 % 3 == 0) {
+    document.write("<p>El numero " + numero1 + " es divisible por 3 </p></br>");
+  }
+  if (numero1 % 5 == 0) {
+    document.write("<p>El numero " + numero1 + " es divisible por 5 </p></br>");
+  }
+  if (numero1 % 7 == 0) {
+    document.write("<p>El numero " + numero1 + " es divisible por 7 </p></br>");
+  }
+} else {
+  document.write(
+    "<p>El numero " + numero1 + " no es divisible por 2,3,5,7 </p></br>"
+  );
+}
